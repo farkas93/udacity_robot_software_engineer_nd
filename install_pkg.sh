4c94408497000a74e2031eb1af5f63a210e7c295
Zsombor Kalotay <zsombor.kalotay@gmx.ch>
2022-04-07 23:08:27 -0700

    upload latest changes

--- install_pkg.sh
+++ install_pkg.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 #Install GMapping
 git clone https://github.com/ros-perception/slam_gmapping ./src/slam_gmapping
 git clone https://github.com/ros-perception/openslam_gmapping ./src/openslam_gmapping
 
+git clone https://github.com/turtlebot/turtlebot ./src/turtlebot
 
+source devel/setup.bash
+rosdep -i install turtlebot_teleop
