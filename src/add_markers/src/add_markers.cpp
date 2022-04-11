59cd437afae2a646e1dad33b15a2659ce43b141d
Zsombor Kalotay <zsombor.kalotay@gmx.ch>
2022-04-11 15:59:22 -0700

    finalize code for final project

--- src/add_markers/src/add_markers.cpp
+++ src/add_markers/src/add_markers.cpp
@@ -1,9 +1,14 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include "add_markers/AddMarker.h"
 
-visualization_msgs::Marker create_marker(double x, double y)
+visualization_msgs::Marker marker;
+
+ros::Publisher marker_pub;
+
+visualization_msgs::Marker create_marker()
 {
-    visualization_msgs::Marker marker;
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
     marker.header.frame_id = "/map";
     marker.header.stamp = ros::Time::now();
@@ -18,15 +23,6 @@ visualization_msgs::Marker create_marker(double x, double y)
 
     // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
     marker.action = visualization_msgs::Marker::ADD;
-
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = x;
-    marker.pose.position.y = y;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
 
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
     marker.scale.x = 0.3;
@@ -43,12 +39,62 @@ visualization_msgs::Marker create_marker(double x, double y)
     return marker;
 }
 
+void send_new_marker_position(double x, double y)
+{
+
+    // Set mode to add since we want to see the message
+    marker.action = visualization_msgs::Marker::ADD;
+
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    marker.pose.position.x = x;
+    marker.pose.position.y = y;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    marker_pub.publish(marker);
+    ROS_INFO("Added the marker. ( %.3f, %.3f)", x, y);
+}
+
+void disable_marker()
+{
+
+    // Simulate picking up the object
+    marker.action = visualization_msgs::Marker::DELETE;
+    marker_pub.publish(marker);
+    ROS_INFO("Deleted the marker. ( %.3f, %.3f)", marker.pose.position.x, marker.pose.position.y);
+}
+
+bool handle_add_marker(add_markers::AddMarker::Request &req, add_markers::AddMarker::Response &res)
+{
+    if (req.add)
+    {
+        send_new_marker_position(req.pos_x, req.pos_y);
+        res.msg_feedback = "Adding a new marker.";
+    }
+    else
+    {
+        disable_marker();
+        res.msg_feedback = "Disabled the marker.";
+    }
+
+    //ROS_INFO(res.msg_feedback);
+    return true;
+}
+
 int main(int argc, char **argv)
 {
+
     ros::init(argc, argv, "add_markers");
     ros::NodeHandle n;
-    ros::Rate r(1);
-    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+    ros::Rate r(10);
+    marker =create_marker();
+
+    marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+    ros::ServiceServer service = n.advertiseService("/add_markers", handle_add_marker);
 
     // Publish the marker
     while (marker_pub.getNumSubscribers() < 1)
@@ -61,15 +107,5 @@ int main(int argc, char **argv)
         sleep(1);
     }
 
-    visualization_msgs::Marker marker = create_marker(3.5, 4.0);
-    marker_pub.publish(marker);
-
-    ros::Duration(5.0).sleep();
-    marker.action = visualization_msgs::Marker::DELETE;
-    marker_pub.publish(marker);
-
-    ros::Duration(5.0).sleep();
-    
-    marker = create_marker(0.0, 4.0);
-    marker_pub.publish(marker);
+    ros::spin();
 }
--- src/pick_objects/src/pick_objects.cpp
+++ src/pick_objects/src/pick_objects.cpp
@@ -1,21 +1,17 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <add_markers/AddMarker.h>
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int main(int argc, char** argv){
-  // Initialize the simple_navigation_goals node
-  ros::init(argc, argv, "pick_objects");
+const int arr_size = 2;
+double positions[arr_size][2] ={{3.5, 4.0}, {0.0, 4.0}};
 
-  //tell the action client that we want to spin a thread by default
-  MoveBaseClient ac("move_base", true);
 
-  // Wait 5 sec for move_base action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
+move_base_msgs::MoveBaseGoal create_goal_msg(double x, double y)
+{
 
   move_base_msgs::MoveBaseGoal goal;
 
@@ -24,40 +20,82 @@ int main(int argc, char** argv){
   goal.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 3.5;
-  goal.target_pose.pose.position.y = 4.0;
+  goal.target_pose.pose.position.x = x;
+  goal.target_pose.pose.position.y = y;
   goal.target_pose.pose.orientation.w = 1.0;
 
   ROS_INFO("Position: (%.2f, %.2f, %.2f)", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y, goal.target_pose.pose.position.z);
+  return goal;
+}
 
-   // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending pickup location");
-  ac.sendGoal(goal);
+int main(int argc, char **argv)
+{
+  // Initialize the simple_navigation_goals node
+  ros::init(argc, argv, "pick_objects");
+  ros::NodeHandle n;
+  ros::Rate r(10);
 
-  // Wait an infinite time for the results
-  ac.waitForResult();
+  // Define a global client that can request services
+  ros::ServiceClient client;
+  client = n.serviceClient<add_markers::AddMarker>("/add_markers");
+  // tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
 
-  ros::Duration(5.0).sleep();
+  // Wait 5 sec for move_base action server to come up
+  while (!ac.waitForServer(ros::Duration(5.0)))
+  {
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+  int goal_ind = 0;
 
-  // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("The robot has picked up the target.");
-  else
-    ROS_INFO("The robot failed to find a valid route to the pickup location.");
+  add_markers::AddMarker marker_request;
+  marker_request.request.pos_x = positions[goal_ind][0];
+  marker_request.request.pos_y = positions[goal_ind][1];
+  marker_request.request.add = true;
+  client.call(marker_request);
+  // TODO: send display marker
 
-
-  // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 0.0;
-  goal.target_pose.pose.position.y = 4.0;
-  goal.target_pose.pose.orientation.w = 1.0;
-
-  ROS_INFO("Position: (%.2f, %.2f, %.2f)", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y, goal.target_pose.pose.position.z);
-
-
-  // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending load-off location");
-  ac.sendGoal(goal);
+  for (; goal_ind < arr_size; ++goal_ind){
 
 
-  return 0;
+    // Send the goal position and orientation for the robot to reach
+    move_base_msgs::MoveBaseGoal goal = create_goal_msg(positions[goal_ind][0], positions[goal_ind][1]);
+    if (goal_ind % 2 == 0)
+      ROS_INFO("Pick-up location sent.");
+    else
+      ROS_INFO("Load-off location sent.");
+    ac.sendGoal(goal);
+
+    // Wait an infinite time for the results
+    ac.waitForResult();
+
+    // Check if the robot reached its goal
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+      if (goal_ind % 2 == 0){
+        ROS_INFO("The robot started picking up the target.");
+        ros::Duration(5.0).sleep();
+        // TODO: pick up the marker
+        marker_request.request.add = false;
+        client.call(marker_request);
+        ROS_INFO("The robot has picked up the target.");
+      }
+      else{
+        //TODO: load off the marker
+
+        ROS_INFO("Load-off location reached");
+        marker_request.request.pos_x = positions[goal_ind][0];
+        marker_request.request.pos_y = positions[goal_ind][1];
+        marker_request.request.add = true;
+        client.call(marker_request);
+
+      }
+      
+
+    }
+    else
+      ROS_INFO("The robot failed to find a valid route to the pickup location.");
+  }
+
+
 }
