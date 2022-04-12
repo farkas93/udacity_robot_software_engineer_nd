41ff20da843c79ed18d4ceca688d546d7a6386ff
kalotayz <zsombor.kalotay@gmx.ch>
2022-04-12 12:15:15 +0200

    correct add_marker.sh

--- src/scripts/add_markers.sh
+++ src/scripts/add_markers.sh
@@ -3,21 +3,21 @@
 xterm  -e  " source devel/setup.bash;
  echo \"Starting GAZEBO WORLD\";
  export ROBOT_INITIAL_POSE='-x -2 -y -2 -z 0 -R 0 -P 0 -Y 0';
- roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/my_robot/worlds/my.world " &
+ roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/my.world " &
 
 sleep 3
 
 xterm -e "source devel/setup.bash;
 echo \"Starting AMCL\";
 source devel/setup.bash;
-roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/src/map/map_savefile.yaml " &
+roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find map)/map_savefile.yaml " &
 
 sleep 3
 
 xterm -e "source devel/setup.bash;
 echo \"Starting View Navigation in RVIZ\";
 source devel/setup.bash;
-roslaunch add_markers rviz.launch rviz_config_file:=$(pwd)/src/rvizConfig/markers.rviz" &
+roslaunch add_markers rviz.launch rviz_config_file:=$(rospack find rvizConfig)/markers.rviz" &
 
 sleep 3
 
